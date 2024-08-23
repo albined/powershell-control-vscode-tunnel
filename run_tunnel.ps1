035e93b0852f82f3d43c249fffb037ea12bcfadf
Albin <albinedegran@gmail.com>
2024-08-23 13:10:06 +0200

    added template etc

--- run_tunnel.ps1
+++ run_tunnel.ps1
@@ -3,7 +3,8 @@
 param (
     [switch]$start,
     [switch]$stop,
-    [switch]$check
+    [switch]$check,
+    [switch]$test
 )
 
 function Execute-Script {
@@ -19,13 +20,15 @@ function Execute-Script {
 
 if ($start) {
     Write-Host "Starting code tunnel..."
-    Execute-Script -scriptPath ".\start_code_tunnel.ps1"
+    Execute-Script -scriptPath "$PSScriptRoot\start_code_tunnel.ps1"
 } elseif ($stop) {
     Write-Host "Stopping code tunnel..."
-    Execute-Script -scriptPath ".\stop_code_tunnel.ps1"
+    Execute-Script -scriptPath "$PSScriptRoot\stop_code_tunnel.ps1"
 } elseif ($check) {
     Write-Host "Checking code tunnel status..."
-    Execute-Script -scriptPath ".\check_code_tunnel_status.ps1"
+    Execute-Script -scriptPath "$PSScriptRoot\check_code_tunnel_status.ps1"
+} elseif ($test) {
+    echo "$PSScriptRoot"
 } else {
-    Write-Host "Please provide a valid option: --start, --stop, or --check"
+    Write-Host "Please provide a valid option: -start, -stop, or -check"
 }
--- start_code_tunnel.ps1
+++ start_code_tunnel.ps1
@@ -1,7 +1,6 @@
 # File: start_code_tunnel.ps1
 
-$tunnelProcess = Start-Process cmd.exe -ArgumentList "/k ping -t google.com" -PassThru
+$tunnelProcess = Start-Process cmd.exe -ArgumentList "/c code tunnel" -PassThru
 $tunnelProcess.Id | Out-File "$PSScriptRoot\tunnel_pid.txt"
 
 
-#$tunnelProcess = Start-Process cmd.exe -ArgumentList "/c code tunnel" -PassThru
